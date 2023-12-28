66a1a9b0df981efdd1e7ef5b01cebfec21c19eb0
ralph <github@ralph-schuster.eu>
2023-12-28 09:35:32 +0000

    Remove rsyslog start

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -137,9 +137,6 @@ configure_spamassassin
 # Configure amavis
 configure_amavis
 
-# Start the syslog
-service rsyslog start
-
 # Make sure amavis can write virusmails
 chown -R amavis:amavis /var/virusmails
 chmod -R u+rw,g+rw /var/virusmails
